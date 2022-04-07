07240b9fcd29ec1432933e8c5fe67dc072172144
Rodrigo <rodvalenciaa@gmail.com>
2022-04-07 16:58:31 -0700

    Finish GET requests to serve page data

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -107,6 +108,19 @@ function validateAnimal(animal) {
     }
     return true;
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
